4e6da9c7d922e63c4fef1dd67e435de482bfa087
Angelina <angry.anggri@gmail.com>
2023-04-25 17:28:06 +0300

    initial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -12,9 +12,20 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIf() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInMillions = {5, -9, 4, 5, 3, 8, 6, 7, 9};
+        long expected = 9;
+        long actual = service.findMax(incomesInMillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
